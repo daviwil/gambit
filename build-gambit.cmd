31d0078170694d519c4c5e22aef4d7ac2af0eb5f
David Wilson <daviwil@github.com>
2020-03-18 16:10:00 -0700

    Clean up

--- build-gambit.cmd
+++ build-gambit.cmd
@@ -3,7 +3,6 @@ REM This script is inspired by the following GitHub issue:
 REM https://github.com/gambit/gambit/issues/480#issuecomment-581215837
 
 setlocal
-dir /s /b "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\"
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64 -vcvars_ver=14.16
 set PATH=c:\tools\msys64\usr\bin;%PATH%
@@ -12,12 +11,6 @@ REM Patch outdated configuration files
 wget -O config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
 wget -O config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
 patch < config.patch
-
-cl
-
-REM MSYS2 should inherit Windows' PATH to find cl.exe
-REM set MSYSTEM=MINGW64
-REM set MSYS2_PATH_TYPE=inherit
 
 sh -c "./configure --enable-single-host --prefix='%CD:\=/%/dist' CC=cl; make -j4; make install"
 
