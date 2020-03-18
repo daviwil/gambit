2dffd3492cb0c54e44523ea6b291cf9eb6a1b28c
David Wilson <daviwil@github.com>
2020-03-18 13:38:41 -0700

    Corrected env script path for VS2015

--- build-gambit.cmd
+++ build-gambit.cmd
@@ -6,7 +6,7 @@ setlocal
 
 REM Set environment variables and restore cwd after vcbuildtools.bat changes it
 set prevdir="%CD%"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+call "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 set PATH=c:\tools\msys64\usr\bin;%PATH%
 cd /d %prevdir%
 
