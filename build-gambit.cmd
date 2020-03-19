c371de601661afd0a90aa14b8f4921703371d11c
David Wilson <daviwil@github.com>
2020-03-18 18:37:59 -0700

    Clean up

--- build-gambit.cmd
+++ build-gambit.cmd
@@ -4,7 +4,7 @@ REM https://github.com/gambit/gambit/issues/480#issuecomment-581215837
 
 setlocal
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64 -vcvars_ver=14.16
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64 -vcvars_ver=14.16
 set PATH=c:\tools\msys64\usr\bin;%PATH%
 
 REM Patch outdated configuration files
@@ -12,10 +12,6 @@ wget -O config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_pla
 wget -O config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
 patch < config.patch
 
-echo "### GIT:"
-sh -c "which git"
-echo ""
-
 sh -c "./configure --enable-single-host --prefix='%CD:\=/%/dist' CC=cl; make -j4; make install"
 
-cat config.log
+REM cat config.log
