4f278e6971ce458024b4bbd4deb8c6cdb667d87a
David Wilson <david@daviwil.com>
2020-04-22 08:43:21 -0700

    Revert "Remove unneeded config file fixes after latest Gambit changes"
    This reverts commit bda70ae3baf72756046b1c31b66ddff4b125722c.

--- .github/workflows/build-gambit.cmd
+++ .github/workflows/build-gambit.cmd
@@ -12,7 +12,9 @@ if "%GITHUB_ACTIONS%" == "true" (
 
 set PATH=c:\tools\msys64\usr\bin;%PATH%
 
-rem Patch makefile to force bootstrapping
+REM Update outdated configuration files and patch makefile
+wget -O config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
+wget -O config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
 patch < .github\workflows\makefile.patch
 
 sh -c "./configure --enable-single-host --prefix='%CD:\=/%/dist' CC=cl; make -j4; make modules; make install"
