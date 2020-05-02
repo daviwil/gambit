35b422ac2aaf3c36d9f7114ec85240c6ef01a92a
David Wilson <david@daviwil.com>
2020-05-02 12:27:14 -0700

    Bootstrap build from earlier commit

--- .github/workflows/build-gambit.sh
+++ .github/workflows/build-gambit.sh
@@ -2,6 +2,18 @@
 
 mkdir -p dist
 
+# Bootstrap the build with an earlier commit
+git checkout 00acf8685
+
+./configure --enable-single-host --prefix=$(pwd)/dist
+make -j4
+make install
+make bootstrap
+
+# Put HEAD back to the branch commit
+git checkout build-gambit-patched
+
+# Build with the bootstrapped compiler
 ./configure --enable-single-host --prefix=$(pwd)/dist
 make -j4
 make modules
