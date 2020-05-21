f261b5462ae848c7c6bb84530d9af6b392d31985
David Wilson <david@daviwil.com>
2020-05-21 04:57:24 -0700

    Try --enable-debug

--- .github/workflows/build-gambit.sh
+++ .github/workflows/build-gambit.sh
@@ -8,16 +8,15 @@ fi
 
 # Bootstrap the build with an earlier commit
 # TODO: Might be able to use $(git merge-base build-gambit build-gambit-patched)
-# BOOTSTRAP_COMMIT=3c4d40de908a
-BOOTSTRAP_COMMIT=ff3056d1e85
+BOOTSTRAP_COMMIT=3c4d40de908a
+#BOOTSTRAP_COMMIT=ff3056d1e85
 git checkout $BOOTSTRAP_COMMIT
 
 echo
 echo "### Bootstrapping Gambit compiler from commit $BOOTSTRAP_COMMIT"
 echo
-./configure --enable-single-host --prefix=$(pwd)/dist CC=$CC
+./configure --enable-single-host --enable-debug --prefix=$(pwd)/dist CC=$CC
 make -j4
-make modules
 make bootstrap
 
 # Put HEAD back to the branch commit
